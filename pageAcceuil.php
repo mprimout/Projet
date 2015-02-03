be037b7f6629ce4a867fea9f93938fc6997dc08a
carolinevern <carolinevernette@yahoo.fr>
2015-02-03 10:55:39 +0100

    Create pageAcceuil.php

---
+++ pageAcceuil.php
@@ -0,0 +1,96 @@
+<!DOCTYPE> <HTML>
+<HEAD>
+	<TITLE>BDD PLANCTON</TITLE>
+</HEAD>
+<BODY> <!--background="image/eau2.jpg" LINK="#0B173B"><!--bgcolor="e9f7f7">a voir VLINK="3399FF" -->
+	<br>
+	<H1><center><FONT size="6">Base de donn&eacute;e planctonique</FONT></center></HI><br>
+	<hr style="color:blue ; height:3px;"/><br><br>
+	<center><TEXT><font face="Arial,Helvetica" size="5">S&eacute;lectionner un plancton ou un groupe de plancton</font></TEXT></center><br>
+	<center><table border=0>
+	<!--<tr><td colspan=2></td><td>-->
+
+	<form method="POST" action="traitementD.php">
+	<SELECT NAME="CHOIX" style="width:130px">
+		<OPTION VALUE="Phyto"SELECTED>Phytoplancton</OPTION>
+		<OPTION VALUE="Zoo">Zooplancton</OPTION>
+	</SELECT></td>
+	</form>
+
+	<!--<td><TEXTAREA NAME="seq" COLS="40" ROWS="1">saisir nom ou sequence</TEXTAREA></td>
+	<td></td><td colspan=2 ><span class="align_right"><input type="submit" value="recherche" style="width:130px"></span></td>
+	</tr>	
+	<tr>
+	<td></td><td></td><td></td><td><input type="button" value="telecharger fichier" style="width:130px"></td>
+	</tr>
+	</table>-->
+	<h2><font size="5">Phytoplancton</font></h2>
+	<table border=5 bordercolor="black">
+	<tr>
+	<td><text><center><a href="cyanobacteries.html"><font color="#0B173B"><b>Cyanobact&eacute;rie</b></font></a></center></text></td>
+	<td><text><center><font color="#0B173B"><b>Diatom&eacute;e</b></font></center></text></td>
+	<td><text><center><font color="#0B173B"><b>Coccolithe</b></font></center></text></td>
+	</tr>
+	<tr>
+	<td><a href="cyanobacteries.html"><img src="image/cyano.jpg" width="240" height="170"></a></td>
+	<td><img src="image/Diatomees.jpg"  width="240" height="170" alt="diat" /></td>
+	<td><img src="image/coccolithe.jpg"   width="240" height="170"alt="coco" /></td>
+	</tr>
+	<tr>
+	<td><text><center><font color="#0B173B"><b>Chysophyc&eacute;e</b></font></center></text></td>
+	<td><text><center><font color="#0B173B"><b>Chlorophyc&eacute;e</b></font></center></text></td>
+	<td><text><center><font color="#0B173B"><b>Dinoflagelle</b></font></center></text></td>
+	</tr>
+	<tr>
+	<td><img src="image/chrysophycees.jpg"  width="240" height="170" alt="chyro" /></td>
+	<td><img src="image/chlorophycees.jpg"  width="240" height="170" alt="chloro" /></td>
+	<td><img src="image/dinoflagelles.jpg"   width="240" height="170"alt="dino" /></td>
+	</tr>
+	</table>
+	<h2><font size="5">Zooplancton</font></h2>
+	<table border=5 bordercolor="black">
+	<tr>
+	<td><text><center><font color="#0B173B"><b>Protozoaire</b></font></center></text></td>
+	<td><text><center><font color="#0B173B"><b>Rotif&egrave;re</b></font></center></text></td>
+	<td><text><center><font color="#0B173B"><b>Cop&eacute;pode</b></font></center></text></td>
+	</tr>
+	<tr>
+	<td><img src="image/protozoaire.jpg"  width="240" height="170" alt="proto" /></td>
+	<td><img src="image/rotifere.jpg"  width="240" height="170" alt="roti" /></td>
+	<td><img src="image/copepode.jpg"   width="240" height="170"alt="cope" /></td>
+	</tr>
+	<tr>
+	<td><text><center><font color="#0B173B"><b>Artemia-salina</b></font></center></text></td>
+	<td><text><center><font color="#0B173B"><b>Le krill</b></font></center></text></td>
+	<td><text><center><font color="#0B173B"><b>Meduse</b></font></center></text></td>
+	</tr>
+	<tr>
+	<td><img src="image/artemia-salina.jpg"  width="240" height="170" alt="art" /></td>
+	<td><img src="image/krill.jpg"  width="240" height="170" alt="krill" /></td>
+	<td><img src="image/meduse.jpg"   width="240" height="170"alt="meduse" /></td>
+	</tr>
+	</table>
+	<h2><font size="5">Zooplancton temporaire</font></h2>
+	<table border=5 bordercolor="black">
+	<tr>
+	<td><text><center><font color="#0B173B"><b>Larve de crustac&eacute;</b></font></center></text></td>
+	<td><text><center><font color="#0B173B"><b>Larve de poisson</b></font></center></text></td>
+	<td><text><center><font color="#0B173B"><b>Larve de mollusque</b></font></center></text></td>
+	</tr>
+	<tr>
+	<td><img src="image/Larve_de_crustace.jpg"  width="240" height="170" alt="crust" /></td>
+	<td><img src="image/LarveThon.jpg"  width="240" height="170" alt="poisson" /></td>
+	<td><img src="image/larve_mollusque.jpg"   width="240" height="170"alt="moll" /></td>
+	</tr>
+	</table> </center>
+	<br><br><br>
+	<font size="4">
+	<text>&nbsp;&nbsp;&nbsp;&nbsp;Quelques liens vers d'autre sites:</text><br><br>
+	<text>&nbsp;&nbsp;&nbsp;&nbsp;</text><a href="http://www.observatoire-plancton.fr/"><font color="#0B173B">L'observatoire du plancton</font></a><br>
+	<text>&nbsp;&nbsp;&nbsp;&nbsp;</text><a href="http://www.planktonchronicles.org/fr/projet"><font color="#0B173B">Chronique du plancton</font></a><br>
+	<text>&nbsp;&nbsp;&nbsp;&nbsp;</text><a href="http://www.plancton-du-monde.org/fr/plancton-du-monde.html"><font color="#0B173B">Plancton du monde</font></a><br>
+	<text>&nbsp;&nbsp;&nbsp;&nbsp;</text><a href="http://www.diatomloir.eu/Siteplancton/Index.html"><font color="#0B173B">Le plancton marin</font></a><br>
+	<text>&nbsp;&nbsp;&nbsp;&nbsp;</text><a href="http://www.planktonchronicles.org/livre/"><font color="#0B173B">Quelques images</font></a>
+	</font>
+</BODY>
+</HTML>
