d015b5bdc5f3956e9484ec6db466a4a855e00c60
marieP <marie.primout@orange.fr>
2015-02-11 20:29:47 +0100

    mise au propre de la page d'accueil sans lien
    voila le titre dit tout

---
+++ accueil.php
@@ -0,0 +1,38 @@
+<!DOCTYPE> 
+<HTML>
+<HEAD>
+	<link rel="stylesheet" href="style.css" />
+	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<TITLE>BDD PLANCTON</TITLE>
+</HEAD>
+<body>
+	<header>
+		<H1><center>Base de donn&eacute;es planctonique
+			<br>
+				<small>petit paragraphe sur le plancton est le role de la base de donnee
+			</font></small>
+			</center>
+		</H1>
+	</header>
+	<br>
+	<br>
+	<br>
+	<br>
+	<div class="col-md-2 col-md-offset-2">.col-md-2 .col-md-offset-2</div>
+	<div class="col-xs-6 col-sm-4">.col-xs-6 .col-sm-4
+		<nav class="navbar navbar-default">
+			<div class="container-fluid">
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav">
+						<li><a href="#">Home</a></li>
+						<li><a href="#">Advanced search</a></li>
+						<li><a href="#">Link</a></li>
+						<li><a href="#">Login admin</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	</div>
+</body>
+
+</HTML>
